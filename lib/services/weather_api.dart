d3f121480d8d114a78dd55a01f47a6c42af39d99
JoNsNoW8 <nmojsijevic4@gmail.com>
2026-02-10 21:59:46 +0100

    fake kljuc

---
+++ lib/services/weather_api.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  final String apiKey = 'kljuc primer zbog gita';
+  final String city = 'Novi Sad'; 
+
+  Future<Map<String, dynamic>> fetchWeather() async {
+    final url = 'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric';
+    
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else {
+        print('API Error Body: ${response.body}');
+        throw Exception('Failed to load weather');
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+}
