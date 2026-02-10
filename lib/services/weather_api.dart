5a5520f1c8444de5c282d985b0bd8980407a321e
JoNsNoW8 <nmojsijevic4@gmail.com>
2026-02-10 22:01:05 +0100

    ne volim git

--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -2,11 +2,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-<<<<<<< HEAD
   final String apiKey = 'kljuc primer zbog gita';
-=======
-  final String apiKey = '4b8447d18269bb63a8ab73d4ca6124e4';
->>>>>>> 59c848e319b4e4f0b9a078ba671a25b7ead52877
+
   final String city = 'Novi Sad'; 
 
   Future<Map<String, dynamic>> fetchWeather() async {
